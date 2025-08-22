d883a8271a97aefb2f91195947a6c75e343348f2
Nawder Loswin <umaywant2@duck.com>
2025-08-22 00:07:12 -0400

    TestingUpdateOfUpdateBat

--- copilot-hippocampus/update.bat
+++ copilot-hippocampus/update.bat
@@ -2,9 +2,12 @@
 for /f "usebackq delims=" %%i in (`git config user.name`) do set GIT_NAME=%%i
 echo Committer: %GIT_NAME%
 echo Commit message: %1
-pause
+
+rem pause
 git add .
-pause
+
+rem pause
 git commit -m "%1"
-pause
+
+rem pause
 git push
