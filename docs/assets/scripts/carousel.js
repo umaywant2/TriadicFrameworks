8817243275e00596107c5393ccac1c6b1195bdd8
Nawder Loswin <umaywant2@duck.com>
2025-09-03 22:52:59 -0400

    Update carousel.js
    Signed-off-by: Nawder Loswin <umaywant2@duck.com>

--- docs/assets/scripts/carousel.js
+++ docs/assets/scripts/carousel.js
@@ -2,3 +2,6 @@ function scrollCarousel(id, direction) {
   const container = document.getElementById(id);
   container.scrollBy({ left: direction * 300, behavior: 'smooth' });
 }
+function toggleTheme() {
+  document.body.classList.toggle('dark-mode');
+}
