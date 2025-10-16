5d43df3f193fa16faef546035fbf6dfd9e363005
Nawder Loswin <umaywant2@duck.com>
2025-10-16 10:03:30 -0400

    Update enTFT.py
    Signed-off-by: Nawder Loswin <umaywant2@duck.com>

--- docs/Coeus/agents/enTFT.py
+++ docs/Coeus/agents/enTFT.py
@@ -1,1 +1,38 @@
+{
+  "coins": [
+    {
+      "id": "coin_001",
+      "name": "PharmaTFT",
+      "type": "priority",
+      "front": "PharmaTFT",
+      "back": "Build a triadic drug interaction simulator using FFF folding and TFT loop logic",
+      "edging": "Predict synergy, prevent catastrophe",
+      "observer_required": true,
+      "encryption": "enTFT",
+      "status": "unminted"
+    },
+    {
+      "id": "coin_002",
+      "name": "ReplicatorFFF",
+      "type": "legacy",
+      "front": "FFF",
+      "back": "Use TFT with FFF to build a symbolic replicator",
+      "edging": "Forci Flui Freqi",
+      "observer_required": true,
+      "encryption": "enTFT",
+      "status": "unminted"
+    },
+    {
+      "id": "coin_003",
+      "name": "ElectroniumRedux",
+      "type": "remix",
+      "front": "Electronium",
+      "back": "Recreate algorithmic music synthesis using triadic scaffolding",
+      "edging": "Echo the unheard",
+      "observer_required": false,
+      "encryption": "enTFT",
+      "status": "unminted"
+    }
+  ]
+}
 
