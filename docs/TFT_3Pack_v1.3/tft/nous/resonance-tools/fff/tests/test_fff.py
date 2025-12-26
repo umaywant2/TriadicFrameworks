a7007cb7e34039e17e668e33b8c60e1ffd7269c2
Nawder Loswin <umaywant2@duck.com>
2025-12-26 18:05:58 -0500

    Update test_fff.py
    Signed-off-by: Nawder Loswin <umaywant2@duck.com>

--- docs/TFT_3Pack_v1.3/tft/nous/resonance-tools/fff/tests/test_fff.py
+++ docs/TFT_3Pack_v1.3/tft/nous/resonance-tools/fff/tests/test_fff.py
@@ -1,5 +1,5 @@
 # Placeholder test for FFF module
 # This file exists only to keep CI green until real tests are added.
 
-def test_fff_placeholder():
-    assert True
+# def test_fff_placeholder():
+#    assert True
