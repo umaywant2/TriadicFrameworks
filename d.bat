d9fd5373dfef83681fc03aa6488e0b04d2e2b29a
Nawder Loswin <umaywant2@duck.com>
2026-01-10 00:03:32 -0500

    Restore files after corruption. Reclaiming resonance.

---
+++ d.bat
@@ -0,0 +1,33 @@
+md overlays
+cd overlays
+copy con README.md
+md earth
+copy con earth\README.md
+cd earth
+md schema
+copy con schema\README.md
+md transforms
+copy con transforms\README.md
+md examples
+copy con examples\README.md
+cd..
+md telescopes
+cd telescopes
+copy con README.md
+md schema
+copy con schema\README.md
+md transforms
+copy con transforms\README.md
+md examples
+copy con README.md
+cd..
+cd..
+md substrate
+cd substrate
+copy con README.md
+md core
+copy con core\README.md
+md operators
+copy con operators\README.md
+md utils
+copy con utils\README.md
