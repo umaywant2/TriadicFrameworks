ba0265b0ece8587eb7fdd96e9940be42741e8288
Nawder Loswin <umaywant2@duck.com>
2026-01-10 00:14:59 -0500

    Create fields.py
    Signed-off-by: Nawder Loswin <umaywant2@duck.com>

---
+++ substrate/core/fields.py
@@ -0,0 +1,17 @@
+"""
+Field containers for phi, V, and R.
+"""
+
+class ScalarField:
+    def __init__(self, data):
+        self.data = data
+
+
+class VectorField:
+    def __init__(self, data):
+        self.data = data
+
+
+class ResonanceField:
+    def __init__(self, data):
+        self.data = data
