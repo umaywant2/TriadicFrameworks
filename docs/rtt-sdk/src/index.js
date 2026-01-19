fbd9be7de579204149f0c31a8571ab7a0d699920
Nawder Loswin <umaywant2@duck.com>
2026-01-19 09:57:28 -0500

    Update index.js
    Signed-off-by: Nawder Loswin <umaywant2@duck.com>

--- docs/rtt-sdk/src/index.js
+++ docs/rtt-sdk/src/index.js
@@ -1,1 +1,14 @@
+// index.js — RTT SDK entrypoint (beta)
+
+import { RTTClient } from "./client.js";
+import * as Beacon from "./beacon.js";
+import * as Profile from "./profile.js";
+import * as Diagnostics from "./diagnostics.js";
+
+export {
+  RTTClient,
+  Beacon,
+  Profile,
+  Diagnostics
+};
 
