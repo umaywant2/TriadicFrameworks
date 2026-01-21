7ddc2702d02b7e89ada048e9e5e3008d2bcc054a
Nawder Loswin <umaywant2@duck.com>
2026-01-21 00:38:10 -0500

    Restore files after corruption. Reclaiming resonance.

---
+++ docs/c.bat
@@ -0,0 +1,30 @@
+@echo off
+echo Creating 14 citation files...
+cd vst_for_large_language_models
+copy con CITATION.cff
+copy con zenodo.json
+cd..
+cd vst_for_protein_language_models/
+copy con CITATION.cff
+copy con zenodo.json
+cd..
+cd vst_for_scientific_simulators
+copy con CITATION.cff
+copy con zenodo.json
+cd..
+cd vst_for_robotics_and_control_policies
+copy con CITATION.cff
+copy con zenodo.json
+cd..
+cd vst_for_embedding_stores_vector_databases
+copy con CITATION.cff
+copy con zenodo.json
+cd..
+cd vst_for_generative_models
+copy con CITATION.cff
+copy con zenodo.json
+cd..
+cd vst_for_multi_model_alignment
+copy con CITATION.cff
+copy con zenodo.json
+cd..
