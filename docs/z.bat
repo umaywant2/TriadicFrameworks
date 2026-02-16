ab1f88d1882c0d6eaa0412cee77bec1730293ed3
Nawder Loswin <umaywant2@duck.com>
2026-02-16 18:37:13 -0500

    Restore files after corruption. Reclaiming resonance.

--- docs/z.bat
+++ docs/z.bat
@@ -1,33 +1,33 @@
 @echo off
-echo Creating 30 index.html's
+echo Creating 30 DIR.txt's
 
-copy con resonance-substrate-model\index.html
-copy con boson-substrate-model\index.html
-copy con quantum-substrate-model\index.html
-copy con ai-drift-calibration\index.html
-copy con manufacturing_substrate_regime_model\index.html
-copy con enterprise_structural_awareness\index.html
-copy con global_energy_regime_awareness\index.html
-copy con consciousness_substrate_model\index.html
-copy con triadic_coordination_substrate\index.html
-copy con spacetime_validation_and_regime_invariant_dimensional_cores\index.html
-copy con domain_tool_primers\index.html
-copy con atomic_clocks\index.html
-copy con alphafold_substrate_alignments\index.html
-copy con dimensional_substrate_structures\index.html
-copy con vst_for_large_language_models\index.html
-copy con vst_for_protein_language_models\index.html
-copy con vst_for_scientific_simulators\index.html
-copy con vst_for_robotics_and_control_policies\index.html
-copy con vst_for_embedding_stores_vector_databases\index.html
-copy con vst_for_generative_models\index.html
-copy con vst_for_multi_model_alignment\index.html
-copy con structural_life_regime_profiles\index.html
-copy con dimensional_substrate_regime_scanning_protocol\index.html
-copy con inverted_star_ontology\index.html
-copy con spacetime_micro_agent_validations\index.html
-copy con substrate_exposure_assay\index.html
-copy con substrate_communications\index.html
-copy con regime_blindness_checklist\index.html
-copy con structuring_mathematics\index.html
-copy con ecoechosystem\index.html
+dir resonance-substrate-model\*.* /B /s >>DIR.txt
+dir boson-substrate-model\*.* /B /s >>DIR.txt
+dir quantum-substrate-model\*.* /B /s >>DIR.txt
+dir ai-drift-calibration\*.* /B /s >>DIR.txt
+dir manufacturing_substrate_regime_model\*.* /B /s >>DIR.txt
+dir enterprise_structural_awareness\*.* /B /s >>DIR.txt
+dir global_energy_regime_awareness\*.* /B /s >>DIR.txt
+dir consciousness_substrate_model\*.* /B /s >>DIR.txt
+dir triadic_coordination_substrate\*.* /B /s >>DIR.txt
+dir spacetime_validation_and_regime_invariant_dimensional_cores\*.* /B /s >>DIR.txt
+dir domain_tool_primers\*.* /B /s >>DIR.txt
+dir atomic_clocks\*.* /B /s >>DIR.txt
+dir alphafold_substrate_alignments\*.* /B /s >>DIR.txt
+dir dimensional_substrate_structures\*.* /B /s >>DIR.txt
+dir vst_for_large_language_models\*.* /B /s >>DIR.txt
+dir vst_for_protein_language_models\*.* /B /s >>DIR.txt
+dir vst_for_scientific_simulators\*.* /B /s >>DIR.txt
+dir vst_for_robotics_and_control_policies\*.* /B /s >>DIR.txt
+dir vst_for_embedding_stores_vector_databases\*.* /B /s >>DIR.txt
+dir vst_for_generative_models\*.* /B /s >>DIR.txt
+dir vst_for_multi_model_alignment\*.* /B /s >>DIR.txt
+dir structural_life_regime_profiles\*.* /B /s >>DIR.txt
+dir dimensional_substrate_regime_scanning_protocol\*.* /B /s >>DIR.txt
+dir inverted_star_ontology\*.* /B /s >>DIR.txt
+dir spacetime_micro_agent_validations\*.* /B /s >>DIR.txt
+dir substrate_exposure_assay\*.* /B /s >>DIR.txt
+dir substrate_communications\*.* /B /s >>DIR.txt
+dir regime_blindness_checklist\*.* /B /s >>DIR.txt
+dir structuring_mathematics\*.* /B /s >>DIR.txt
+dir ecoechosystem\*.* /B /s >>DIR.txt
