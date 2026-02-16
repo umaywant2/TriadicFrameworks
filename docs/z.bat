d474853284f262802802870e3a00a5a3720f7c53
Nawder Loswin <umaywant2@duck.com>
2026-02-16 18:24:35 -0500

    Restore files after corruption. Reclaiming resonance.

---
+++ docs/z.bat
@@ -0,0 +1,33 @@
+@echo off
+echo Creating 30 index.html's
+
+copy con resonance-substrate-model\index.html
+copy con boson-substrate-model\index.html
+copy con quantum-substrate-model\index.html
+copy con ai-drift-calibration\index.html
+copy con manufacturing_substrate_regime_model\index.html
+copy con enterprise_structural_awareness\index.html
+copy con global_energy_regime_awareness\index.html
+copy con consciousness_substrate_model\index.html
+copy con triadic_coordination_substrate\index.html
+copy con spacetime_validation_and_regime_invariant_dimensional_cores\index.html
+copy con domain_tool_primers\index.html
+copy con atomic_clocks\index.html
+copy con alphafold_substrate_alignments\index.html
+copy con dimensional_substrate_structures\index.html
+copy con vst_for_large_language_models\index.html
+copy con vst_for_protein_language_models\index.html
+copy con vst_for_scientific_simulators\index.html
+copy con vst_for_robotics_and_control_policies\index.html
+copy con vst_for_embedding_stores_vector_databases\index.html
+copy con vst_for_generative_models\index.html
+copy con vst_for_multi_model_alignment\index.html
+copy con structural_life_regime_profiles\index.html
+copy con dimensional_substrate_regime_scanning_protocol\index.html
+copy con inverted_star_ontology\index.html
+copy con spacetime_micro_agent_validations\index.html
+copy con substrate_exposure_assay\index.html
+copy con substrate_communications\index.html
+copy con regime_blindness_checklist\index.html
+copy con structuring_mathematics\index.html
+copy con ecoechosystem\index.html
