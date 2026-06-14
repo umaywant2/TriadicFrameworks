4f2644d627d45f829381257ccfccfe510ecc6527
Nawder Loswin <umaywant2@duck.com>
2026-06-14 11:50:30 -0400

    Restore files after corruption. Reclaiming resonance.

---
+++ docs/rtt/guild/l.bat
@@ -0,0 +1,68 @@
+@echo off
+echo Creating... 
+md Little_Science_Series
+cd Little_Science_Series
+md Book_1_Little_Gravity
+cd Book_1_Little_Gravity
+echo . > 01_Scene_Design.md
+echo . > 02_Supporting_Details.md
+echo . > 03_Scenery_and_Visuals.md
+echo . > 04_Animation_Beats.md
+echo . > 05_Imagine_Scripts_Stills.md
+echo . > 06_Imagine_Scripts_Animations.md
+echo . > 07_Character_Metadata_LittleGravity.json
+echo . > README.md
+cd..
+md Book_2_Little_Light
+cd Book_2_Little_Light
+echo . > 01_Scene_Design.md
+echo . > 02_Supporting_Details.md
+echo . > 03_Scenery_and_Visuals.md
+echo . > 04_Animation_Beats.md
+echo . > 05_Imagine_Scripts_Stills.md
+echo . > 06_Imagine_Scripts_Animations.md
+echo . > 07_Character_Metadata_LittleLight.json
+echo . > README.md
+cd..
+md Book_3_Little_Motion
+cd Book_3_Little_Motion
+echo . > 01_Scene_Design.md
+echo . > 02_Supporting_Details.md
+echo . > 03_Scenery_and_Visuals.md
+echo . > 04_Animation_Beats.md
+echo . > 05_Imagine_Scripts_Stills.md
+echo . > 06_Imagine_Scripts_Animations.md
+echo . > 07_Character_Metadata_LittleMotion.json
+echo . > README.md
+cd..
+md Book_4_Little_Time
+cd Book_4_Little_Time
+echo . > 01_Scene_Design.md
+echo . > 02_Supporting_Details.md
+echo . > 03_Scenery_and_Visuals.md
+echo . > 04_Animation_Beats.md
+echo . > 05_Imagine_Scripts_Stills.md
+echo . > 06_Imagine_Scripts_Animations.md
+echo . > 07_Character_Metadata_LittleTime.json
+echo . > README.md
+cd..
+md Book_5_Little_Matter
+cd Book_5_Little_Matter
+echo . > 01_Scene_Design.md
+echo . > 02_Supporting_Details.md
+echo . > 03_Scenery_and_Visuals.md
+echo . > 04_Animation_Beats.md
+echo . > 05_Imagine_Scripts_Stills.md
+echo . > 06_Imagine_Scripts_Animations.md
+echo . > 07_Character_Metadata_LittleMatter.json
+echo . > README.md
+cd..
+md Series_Metadata
+cd Series_Metadata
+echo . > Characters_Index.json
+echo . > Visual_Identity_Guide.md
+echo . > Animation_Identity_Guide.md
+echo . > Apprentice_Guild_Lore.md
+echo . > README.md
+cd..
+cd..
